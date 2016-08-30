50e0d88aee372fb4a256253c15298b7b8a620d6c
roshniv <heyitsroshni@gmail.com>
2016-08-30 16:26:18 +0530

    Select complete Data within input box in a single click

---
+++ src/SelectData.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class CopyReferralLink extends React.Component {
+
+	render() {
+		
+		return (
+			<div>
+				<p>{this.props.text ? this.props.text : 'Click here to copy'}</p>
+				<input type="text"
+				value={'abcd'}
+				onClick={(e)=>{e.target ? e.target.setSelectionRange(0, e.target.value.length) : null}}
+				/>
+			</div>
+		);
+	}
+}
